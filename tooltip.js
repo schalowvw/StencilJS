3a58416fe95d930dbac4931ea14472de9bb7b586
Sebastian Schalow <sebastian.schalow@volkswagen.de>
2020-10-06 16:59:01 +0200

    Exten tooltip with event listeners

--- tooltip.js
+++ tooltip.js
@@ -1,13 +1,26 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
+    this._tooltipContainer;
   }
 
   connectedCallback() {
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip);
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip);
     this.appendChild(tooltipIcon);
   }
+
+  _showTooltip = () => {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = 'This is the tooltip text';
+    this.appendChild(this._tooltipContainer);
+  };
+
+  _hideTooltip = () => {
+    this.removeChild(this._tooltipContainer);
+  };
 }
 
 customElements.define('uc-tooltip', Tooltip, {});
